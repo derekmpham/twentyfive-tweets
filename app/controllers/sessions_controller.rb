9031f858eb166bfa0c5cd89a1ae13442e697da48
Derek Pham <derekmpham@gmail.com>
2017-06-07 17:03:19 -0700

    Work on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+	include SessionsHelper
+
+  def new
+  end
+
+  def create
+  	@user = User.find_by_username(params[:username])
+  	if @user && @user.authenticate(params[:password])
+  	  login(@user)
+  	  redirect_to "/"
+  	else
+  	  redirect_to '/login'
+  	end
+  end
+
+  def destroy
+  	logout
+  	redirect_to '/'
+  end
 end
