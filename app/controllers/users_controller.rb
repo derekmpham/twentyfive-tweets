7b964db87a600db9d2f528e82faf5d96e51b1f24
Derek Pham <derekmpham@gmail.com>
2017-06-07 23:13:27 -0700

    Work on users controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+	include SessionsHelper
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	@user.password = params[:user][:password]
+    @recommendations = Recommendation.all
+  	if @user.save
+  	  session[:user_id] = @user.id
+  	  redirect_to root_path
+  	else
+  	  redirect_to '/users/new'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :username, :password)
+  end
 end
