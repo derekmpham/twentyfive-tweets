3524a33ee8c8e3935bcfc28b9ed283d0aacf4c8e
Derek Pham <derekmpham@gmail.com>
2017-06-08 10:38:16 -0700

    Add bootstrap style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	@user.password = params[:user][:password]
-    @recommendations = Recommendation.all
   	if @user.save
   	  session[:user_id] = @user.id
   	  redirect_to root_path
