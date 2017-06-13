2a576ef9cfe41bb2c40068056e499c198f411d1c
Derek Pham <derekmpham@gmail.com>
2017-06-13 10:39:06 -0700

    Testing: remove twitter credentials in controller files

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,6 @@
 class TweetsController < ApplicationController
 
   def index
-    $client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-    end
     @trump_tweets = $client.search("from:realDonaldTrump", result_type: "recent").take(5)
     render "index"
   end
