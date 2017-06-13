06b42029f3d1f14e19b182fc2ba6f66bd38353ab
Derek Pham <derekmpham@gmail.com>
2017-06-13 10:02:24 -0700

    Move twitter credentials take 2

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,13 @@
 class TweetsController < ApplicationController
 
   def index
-  	@trump_tweets = $client.search("from:realDonaldTrump", result_type: "recent").take(5)
+    $client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+    @trump_tweets = $client.search("from:realDonaldTrump", result_type: "recent").take(5)
     render "index"
   end
 
